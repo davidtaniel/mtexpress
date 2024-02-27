d1dc15a175899eceade57d8f1ec36aef8364271e
jacaranda <jacarandaalfaro@outlook.com>
2024-02-27 10:33:35 -0600

    adding initial files

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const layouts = require('express-ejs-layouts');
+app.set('view engine', 'ejs')
+app.use(layouts);
+app.use(express.static("public"))
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.render('index')
 })
 app.listen(process.env.PORT || 3000)
